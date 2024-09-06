525a70581ae9f46bff0fe96dc90524d7cc08e876
Ajeet Kumar Sah <aminemshaw@gmail.com>
2024-09-06 17:36:16 +0530

    status changed

--- controllers/userController.js
+++ controllers/userController.js
@@ -3,7 +3,7 @@ const userService = require('../services/userService');
 exports.getUserProfile = async (req, res, next) => {
   try {
     const user = await userService.getUserById(req.user.id);
-    res.status(200).json({ success: true, data: user });
+    res.status(200).json({ status: true, data: user });
   } catch (error) {
     next(error);
   }
@@ -12,7 +12,7 @@ exports.getUserProfile = async (req, res, next) => {
 exports.updateUserProfile = async (req, res, next) => {
   try {
     const user = await userService.updateUser(req.user.id, req.body);
-    res.status(200).json({ success: true, data: user });
+    res.status(200).json({ status: true, data: user });
   } catch (error) {
     next(error);
   }
@@ -20,7 +20,7 @@ exports.updateUserProfile = async (req, res, next) => {
 exports.getUsers = async (req, res, next) => {
   try {
     const user = await userService.getUsers();
-    res.status(200).json({ success: true, data: user });
+    res.status(200).json({ status: true, data: user });
   } catch (error) {
     next(error);
   }
@@ -31,10 +31,10 @@ exports.getUserById = async (req, res, next) => {
     const user = await userService.getUserById(req.user.id);
 
     if (!user) {
-      return res.status(404).json({ success: false, message: 'User not found' });
+      return res.status(404).json({ status: false, message: 'User not found' });
     }
 
-    res.status(200).json({ success: true, data: user });
+    res.status(200).json({ status: true, data: user });
   } catch (error) {
     next(error);
   }
