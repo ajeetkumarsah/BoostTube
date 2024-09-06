55c2b760806aa9ef5cc1c38cddcf45a093a6f1f0
Ajeet Kumar Sah <aminemshaw@gmail.com>
2024-09-06 15:59:33 +0530

    user fixed

--- controllers/userController.js
+++ controllers/userController.js
@@ -27,7 +27,13 @@ exports.getUsers = async (req, res, next) => {
 };
 exports.getUserById = async (req, res, next) => {
   try {
-    const user = await userService.getUsers({user:req.user.id});
+    // Fetch user by the specific ID
+    const user = await userService.getUserById(req.user.id);
+
+    if (!user) {
+      return res.status(404).json({ success: false, message: 'User not found' });
+    }
+
     res.status(200).json({ success: true, data: user });
   } catch (error) {
     next(error);
